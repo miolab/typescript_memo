e8145e4ba8ad1f1c48785d93934f4862c62fb383
im <poson_hemp@yahoo.co.jp>
2020-06-03 18:48:21 +0900

    update docs

--- compile_notes/compile_all_at_once.js
+++ compile_notes/compile_all_at_once.js
@@ -1,7 +1,8 @@
-"use strict";
-// ディレクトリ内の複数tsファイルを、一気にコンパイルする
+// * ディレクトリ内の複数tsファイルを、一気にコンパイルする
 // `$ tsc --init` 実行（→ tsconfig.json が生成される）
-// `$ tsc` 実行（→ tsconfig.json を見に行き、その内容に沿ってコンパイルが行われる）
+// `$ tsc` 実行
+// （→ tsconfig.json を見に行き、その内容に沿ってコンパイルが行われる。）
+// （`tsc hoge.ts` みたいにファイル指定すると、tsconfig.json の設定は無視される）
 // `-w` オプションも付帯可
 var testMsg = 'test: compile all at once.';
 console.log(testMsg);
--- compile_notes/compile_all_at_once.ts
+++ compile_notes/compile_all_at_once.ts
@@ -1,6 +1,8 @@
-// ディレクトリ内の複数tsファイルを、一気にコンパイルする
+// * ディレクトリ内の複数tsファイルを、一気にコンパイルする
 // `$ tsc --init` 実行（→ tsconfig.json が生成される）
-// `$ tsc` 実行（→ tsconfig.json を見に行き、その内容に沿ってコンパイルが行われる）
+// `$ tsc` 実行
+// （→ tsconfig.json を見に行き、その内容に沿ってコンパイルが行われる。）
+// （`tsc hoge.ts` みたいにファイル指定すると、tsconfig.json の設定は無視される）
 // `-w` オプションも付帯可
 
 let testMsg: string = 'test: compile all at once.';
