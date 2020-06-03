f39de9c59d68e2da9ff77a1bf9073b60c5410fa4
im <poson_hemp@yahoo.co.jp>
2020-06-03 18:09:52 +0900

    ts | js files move into dir

---
+++ compile_notes/tsc_watch.js
@@ -0,0 +1,6 @@
+// * Watchモード
+//（自動化ユニットテストみたいに自動でコンパイルしてくれるモード）
+// tsc tsc_watch.ts --watch
+// tsc tsc_watch.ts --w
+var greeting = 'Hello, watch! hoge';
+console.log(greeting);
---
+++ compile_notes/tsc_watch.ts
@@ -0,0 +1,7 @@
+// * Watchモード
+//（自動化ユニットテストみたいに自動でコンパイルしてくれるモード）
+// tsc tsc_watch.ts --watch
+// tsc tsc_watch.ts --w
+
+let greeting: string = 'Hello, watch! hoge';
+console.log(greeting);
